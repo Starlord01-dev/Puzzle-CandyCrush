d3771b1727b8166701e59f02a9fda77aafb06348
Starlord01-dev <59503489+Starlord01-dev@users.noreply.github.com>
2021-06-23 19:49:03 +0300

    Added level Data

--- Assets/Scripts/Collapse/LevelData.cs
+++ Assets/Scripts/Collapse/LevelData.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class LevelData : MonoBehaviour
+[System.Serializable]
+public class LevelData
 {
-    // Start is called before the first frame update
-    void Start()
+    int width;
+    int height;
+    int[,] Databoard;
+
+    LevelData(EditorBoard board)
     {
-        
+        width = board.width;
+        height = board.height;
+
+        Databoard = new int[width, height];
+
+        for(int i = 0; i< board.width; i++)
+        {
+            for(int j = 0; j<board.height; j++)
+            {
+                switch (board.Popables[i, j].tag) 
+                {
+                    case "BlueEyesBall":
+                        Databoard[i, j] = 1;
+                        break;
+                    case "RedEyesBox":
+                        Databoard[i, j] = 2;
+                        break;
+                    case "PyramidBlue":
+                        Databoard[i, j] = 3;
+                        break;
+                    case "Coin":
+                        Databoard[i, j] = 0;
+                        break;
+                    case "Obstacle":
+                        Databoard[i, j] = -1;
+                        break;
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
